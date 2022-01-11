62840ae5418832817ff258e60ab7bd5077a89fa0
AbrihaDavid <abrihadavid@gmail.com>
2022-01-11 16:04:20 +0100

    Daily task: done

---
+++ src/test/java/day02/MathAlgorithmsTest.java
@@ -0,0 +1,16 @@
+package day02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathAlgorithmsTest {
+
+    @Test
+    void testGreatestCommonDivisor(){
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
+
+        assertEquals(6,mathAlgorithms.greatestCommonDivisor(12,18));
+    }
+
+}
